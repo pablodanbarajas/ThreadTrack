14ff6512b7554f80f7ee4c08c21f465ba10f6574
Pablo Daniel Lara Barajas <pablo.lara6279@alumnos.udg.mx>
2026-02-26 10:06:59 -0600

    Remove unused Navigate import

--- src/pages/GarmentDetail.tsx
+++ src/pages/GarmentDetail.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useParams, Link, Navigate } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { Loader, AlertCircle, Home, Download, Copy, ArrowLeft } from 'lucide-react'
 import QRCode from 'qrcode.react'
 import { garmentService } from '../services/garmentService'
